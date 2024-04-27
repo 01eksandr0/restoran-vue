1f1cddc293605df90071ffb0bbb0d08d27496af5
01eksandr0 <krux228@gmail.com>
2024-04-27 17:27:30 +0300

    Update NavBar.vue

--- src/modules/Header/NavBar.vue
+++ src/modules/Header/NavBar.vue
@@ -7,7 +7,7 @@
           href="https://www.instagram.com/nashi_sushi_bar/?igshid=NzAzN2Q1NTE%3D"
           target="_blank"
           ><svg>
-            <use :href="icons + '#icon-instagram'"></use>
+            <use xlink:href="../../shared/img/icons.svg#icon-instagram"></use>
           </svg>
         </a>
       </li>
@@ -15,14 +15,14 @@
         <router-link class="nav-link"
           >Кошик
           <svg>
-            <use :href="icons + '#icon-cart'"></use>
+            <use xlink:href="../../shared/img/icons.svg#icon-cart"></use>
           </svg>
         </router-link>
       </li>
       <li class="menu-btn">
         <button class="nav-link" @click="openMobailMenu">
           <svg height="20" width="20">
-            <use :href="icons + '#icon-menu'"></use>
+            <use xlink:href="../../shared/img/icons.svg#icon-menu"></use>
           </svg>
         </button>
       </li>
@@ -31,15 +31,9 @@
 </template>
 
 <script>
-import icons from "../../shared/img/icons.svg";
 export default {
   props: {
     openMobailMenu: Function,
-  },
-  data() {
-    return {
-      icons,
-    };
   },
 };
 </script>
