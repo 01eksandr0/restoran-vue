99d727b3848e022b550cf498fda0375aed42ea09
01eksandr0 <krux228@gmail.com>
2024-04-28 12:02:40 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import svgLoader from "vite-svg-loader";
 import { globalStylesOptions } from "./global.styles";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), svgLoader()],
+  plugins: [vue()],
   css: {
     preprocessorOptions: {
       scss: {
